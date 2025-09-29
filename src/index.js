b10ab880e7137e8c4a890e5d29824251b44032e7
school120 <daniel.steinerman@frisch.org>
2025-09-29 15:35:17 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-import "./index.css";
 import App from "./App";
 
-const container = document.getElementById("root")!;
-const root = createRoot(container);
-root.render(<App />);
+const el = document.getElementById("root");
+createRoot(el).render(<App />);
