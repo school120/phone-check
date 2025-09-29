d86a87ec98fadcfc672e63d2c156814ac798d8ea
school120 <daniel.steinerman@frisch.org>
2025-09-29 15:38:47 -0400

    Delete vite.config.ts

--- vite.config.ts
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-
-// IMPORTANT: base must match your GitHub Pages repo name
-export default defineConfig({
-  plugins: [react()],
-  base: "/phone-check/",  // <-- ensures assets resolve on school120.github.io/phone-check/
-});
